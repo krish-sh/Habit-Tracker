bf236ad1c88a0775c2f54ecc08f3b46311630490
Krish Sharma <krishsharma3355@gmail.com>
2025-11-24 22:30:50 +0530

    Frontend Url added

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://habit-tracker-seven-beta.vercel.app/",
     credentials: true,
   })
 );
